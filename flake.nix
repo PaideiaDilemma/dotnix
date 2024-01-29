89080bb3dda1fc57769846e220cd586b8e5a3224
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-01-29 18:47:31 +0100

    add nixos-hardware flake

--- flake.nix
+++ flake.nix
@@ -26,6 +26,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     # Simplify once lazy trees are available https://github.com/NixOS/nix/pull/6530
     wlclipmgr.url = "git+https://www.github.com/PaideiaDilemma/wlclipmgr?submodules=1";
 
