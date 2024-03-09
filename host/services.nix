d534af7a4a45f6bafeee5eebde03efbb72c10880
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-03-09 14:44:58 +0100

    fix greetd

--- host/services.nix
+++ host/services.nix
@@ -13,21 +13,12 @@ in
   };
 
   config = {
-    #Evremap service to fix my keyboard layout..
-    #systemd.services.keyboard-remaps = {
-    #  description = "Remaps keys with evremap";
-    #  serviceConfig.ExecStart = toString (pkgs.writeShellScript "remap" ''
-    #    /home/max/Apps/Files/evremap/target/release/evremap remap /home/max/Apps/Files/evremap/de.toml
-    #  '');
-    #  wantedBy = [ "multi-user.target" ];
-    #  serviceConfig.group = "root";
-    #};
     services.greetd = {
       enable = true;
       settings = rec {
         hyprland_session = {
           user = "max";
-          command = "$SHELL -l ${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
+          command = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
         };
         shell_session = {
           user = "max";
