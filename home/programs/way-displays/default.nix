7661487431d6a29143721b57c6ebff34ecf8e847
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-03-26 16:24:17 +0100

    fix way-displays

--- home/programs/way-displays/default.nix
+++ home/programs/way-displays/default.nix
@@ -9,15 +9,19 @@ ORDER:
   - Samsung Electric Company S22D300 0x30333635
   - LG Electronics LG ULTRAGEAR 110MASXPXJ78
   - Samsung Electric Company U28D590
-  - Philips Consumer Electronics Company PHL 275V8 UK02239005336
+  - 'Philips Consumer Electronics Company PHL 275V8 UK\d+'
+  - '!.*$'
+  - 'eDP-1'
 SCALE:
   - NAME_DESC: Najing CEC Panda FPD Technology CO. ltd 0x0050
     SCALE: 1.25
   - NAME_DESC: InfoVision Optoelectronics (Kunshan) Co.,Ltd China 0x34D1
     SCALE: 1.25
 MODE:
-  - NAME_DESC: Philips Consumer Electronics Company PHL 275V8 UK02239005336
+  - NAME_DESC: 'Philips Consumer Electronics Company PHL 275V8 UK\d+'
     MAX: TRUE
+VRR_OFF:
+  - 'eDP-1'
   '';
 
   home.packages = with pkgs; [
