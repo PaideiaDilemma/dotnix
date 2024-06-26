c78d903b109d8cd241f267cf9f89f78bb086fd0f
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-06-26 16:35:29 +0200

    hyprlock testing

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     };
 
     hyprlock = {
-      url = "github:hyprwm/hyprlock";
+      url = "github:PaideiaDilemma/hyprlock";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.hyprlang.follows = "hyprland";
     };
