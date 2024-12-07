791b536aa96637b959253a44d478a53354c4cdf1
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-12-07 17:05:41 +0100

    add syncthing

---
+++ home/programs/syncthing/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.hyprhome;
+  colors = config.colors;
+in {
+  options.hyprhome.syncthing = {
+    enable = mkOption {
+      default = true;
+      description = "Whether to enable file sync.";
+      type = types.bool;
+    };
+  };
+
+  config = mkIf (cfg.syncthing.enable) {
+    services.syncthing = {
+      enable = true;
+      tray = true;
+    };
+  };
+}
