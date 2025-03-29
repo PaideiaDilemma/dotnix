ad37c8800e7715b06009a8320f92ec15180c1e79
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-03-29 12:27:58 +0100

    add audio interface config

--- host/variants/desktop.nix
+++ host/variants/desktop.nix
@@ -5,4 +5,19 @@
   services.ollama.rocmOverrideGfx = "10.3.0";
   #services.ollama.acceleration = "rocm";
   host.localllm.enable = true;
+
+  services.pipewire.wireplumber.extraConfig = {
+    "51-scarlet-solo" = {
+      "monitor.alsa.rules" = [ {
+        matches = [ {
+          "device.name" = "~alsa_card.usb-Focusrite*";
+        } ];
+        actions = {
+          update-props = {
+            "api.acp.probe-rate" = 44100;
+          };
+        };
+      } ];
+    };
+  };
 }
