95dea201e2c851ab2a11da1df4db7f4f5bbdebf5
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-04-19 10:04:13 +0200

    add script to remove nix-env when in a dirstobox

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -21,9 +21,16 @@
   cava-internal = pkgs.writeShellScriptBin "cava-internal" ''
     cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
   '';
+
+  # I use this for disrobox
+  remove-nix-from-path = pkgs.writeShellScriptBin "remove-nix-from-path" ''
+    export PATH=$(echo $PATH | sed -e 's|/nix/[^:]*||g')
+  '';
+
 in {
   home.packages = with pkgs; [
     hyprsetwallpaper
     cava-internal
+    remove-nix-from-path
   ];
 }
