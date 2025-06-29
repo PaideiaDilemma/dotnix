02d4b13663bb1672c010ef2b912eb4075d3306f4
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-06-29 13:09:28 +0200

    fix mako

--- home/programs/mako/default.nix
+++ home/programs/mako/default.nix
@@ -22,14 +22,14 @@ in {
       settings = {
         font = "Noto Sans 10";
         anchor = "bottom-center";
-        backgroundColor = "${colors.base.sky_}";
-        textColor = "${colors.base.sun}";
+        background-color = "${colors.base.sky_}";
+        text-color = "${colors.base.sun}";
         margin = "2";
         #outerMargin = 5;
-        borderColor = "${colors.base.sun}";
-        borderSize = 1;
-        borderRadius = 6;
-        progressColor = "over ${colors.six.red}CC";
+        border-color = "${colors.base.sun}";
+        border-size = 1;
+        border-radius = 6;
+        progress-color = "over ${colors.six.red}CC";
         icons = true;
       };
     };
