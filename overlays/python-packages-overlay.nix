b9804db4b98efb7b5c32909a0423e2f694304b16
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-08-02 15:07:32 +0200

    fix pwntools debugger

--- overlays/python-packages-overlay.nix
+++ overlays/python-packages-overlay.nix
@@ -14,7 +14,12 @@ final: prev: {
               hash = "sha256-/1ZO6j4Xex1DZ+gbTnu8bghgsutAAdhjVk8vbRM0gKE=";
             };
 
-            debugger = final.pwndbg;
+            debugger = final.pwndbg; # No worky?
+
+            postFixup = final.lib.optionalString (!final.stdenv.hostPlatform.isDarwin) ''
+              mkdir -p "$out/bin"
+              makeWrapper "${final.pwndbg}/bin/pwndbg" "$out/bin/pwntools-gdb"
+            '';
           });
       })
     ];
