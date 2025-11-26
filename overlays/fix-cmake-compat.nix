79467bfc3be8dff88cd785f1546c5b2f4f010a4e
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-11-26 10:36:35 +0100

    updato

--- overlays/fix-cmake-compat.nix
+++ overlays/fix-cmake-compat.nix
@@ -5,16 +5,17 @@ final: prev: {
       urls = [
         "https://curl.haxx.se/download/curl-${version}.tar.xz"
         "https://github.com/curl/curl/releases/download/curl-${
-          builtins.replaceStrings [ "." ] [ "_" ] version
+          builtins.replaceStrings ["."] ["_"] version
         }/curl-${version}.tar.xz"
       ];
       hash = "sha256-9GGaHiR0xLv+3IinwhkSCcgzS0j6H05T/VhMwS6RIN0=";
     };
   };
-  cmake3 = (prev.cmake.override {
+  cmake3 =
+    (prev.cmake.override {
       curlMinimal = final.curlMinimal8_14_1;
     }).overrideAttrs (old: rec {
-    patches = [
+      patches = [
         # Add NIXPKGS_CMAKE_PREFIX_PATH to cmake which is like CMAKE_PREFIX_PATH
         # except it is not searched for programs
         ./cmake-compat-patches/000-nixpkgs-cmake-prefix-path.diff
@@ -25,20 +26,22 @@ final: prev: {
         ./cmake-compat-patches/008-FindCURL-Add-more-target-properties-from-pkg-config.diff
         # Backport of https://gitlab.kitware.com/cmake/cmake/-/commit/1b0c92a3a1b782ff3e1c4499b6ab8db614d45bcd
         ./cmake-compat-patches/009-cmCurl-Avoid-using-undocumented-type-for-CURLOPT_NETRC-values.diff
-    ];
-    version = "3.31.6";
+      ];
+      version = "3.31.6";
 
-    src = prev.fetchurl {
-      url = "https://cmake.org/files/v${prev.lib.versions.majorMinor version}/cmake-${version}.tar.gz";
-      hash = "sha256-ZTQn8PUBR1Cq//InJ/sqpgxscyypGAjPt4ziLd2eVfA=";
-    };
-  });
+      src = prev.fetchurl {
+        url = "https://cmake.org/files/v${prev.lib.versions.majorMinor version}/cmake-${version}.tar.gz";
+        hash = "sha256-ZTQn8PUBR1Cq//InJ/sqpgxscyypGAjPt4ziLd2eVfA=";
+      };
+    });
   keystone = prev.keystone.override {
     cmake = final.cmake3;
   };
   catimg = prev.catimg.overrideAttrs (old: {
-    cmakeFlags = old.cmakeFlags or [] ++ prev.lib.mapAttrsToList prev.lib.cmakeFeature {
-      CMAKE_POLICY_VERSION_MINIMUM = "3.5";
-    };
+    cmakeFlags =
+      old.cmakeFlags or []
+      ++ prev.lib.mapAttrsToList prev.lib.cmakeFeature {
+        CMAKE_POLICY_VERSION_MINIMUM = "3.5";
+      };
   });
 }
